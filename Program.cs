a2a5c40efa2b53fb26e40a38ddd87652ffde9e49
Harlean Kaur <21HKaur@heckgrammar.co.uk>
2024-06-25 15:15:51 +0100

    commit2 25.06.24

--- Program.cs
+++ Program.cs
@@ -12,13 +12,23 @@ namespace Y9_DEC_TO_BIN_SKELETON
         {
             //STARTER: Practice using breakpoints and the watch window (F8 to single-step, F11 to start in single step mode)
             int myInt = 0; //just for testing single stepping
-            string myString = "12"; //watch me being cast from string to int
-            int myStringAsInt = Convert.ToInt32(myString); //watch me cast from string to int
+            string myString1 = "12"; //watch me being cast from string to int
+            int myStringAsInt = Convert.ToInt32(myString1); //watch me cast from string to int
               
             //MAIN:  NUMBER CONVERSION PROGRAM
 
             //CODE GOES HERE
             Console.WriteLine(numberConversion(48, 2)); ///THIS CALLS AND OUTPUTS THE RESULT OF A SUBROUTINE
+
+            int myDecimal = Console.ReadLine();
+            int myInteger = 0;
+            int myBinaryDig = 0;
+            int myBinaryNum = 0; 
+            while (myDeci)
+
+            string myString2 = Console.ReadLine();
+            string newString = myString1.Insert(0, myString2); ///this inserts myString2 at the beginnging on myString1
+            Console.WriteLine(newString);
         }
 
         //LET'S USE THE 'STRUCTURED APPROACH' TO PROGRAMMING... A QUICK INTRODUCTION TO SUBROUTINES(functions and procedures)...a.k.a
