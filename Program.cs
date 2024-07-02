6e73371d667612e49e3986d6645de4d32dc9a290
Harlean Kaur <21HKaur@heckgrammar.co.uk>
2024-07-02 15:04:47 +0100

    commit1 02.07.24

--- Program.cs
+++ Program.cs
@@ -11,24 +11,39 @@ namespace Y9_DEC_TO_BIN_SKELETON
         static void Main(string[] args)
         {
             //STARTER: Practice using breakpoints and the watch window (F8 to single-step, F11 to start in single step mode)
-            int myInt = 0; //just for testing single stepping
-            string myString1 = "12"; //watch me being cast from string to int
-            int myStringAsInt = Convert.ToInt32(myString1); //watch me cast from string to int
+            //int myInt = 0; //just for testing single stepping
+            //string myString1 = "12"; //watch me being cast from string to int
+            //int myStringAsInt = Convert.ToInt32(myString1); //watch me cast from string to int
               
             //MAIN:  NUMBER CONVERSION PROGRAM
 
             //CODE GOES HERE
-            Console.WriteLine(numberConversion(48, 2)); ///THIS CALLS AND OUTPUTS THE RESULT OF A SUBROUTINE
-
-            int myDecimal = Console.ReadLine();
+            //Console.WriteLine(numberConversion(48, 2)); ///THIS CALLS AND OUTPUTS THE RESULT OF A SUBROUTINE
+           
+            Console.WriteLine("Please input decimal number from 0 to 255: "); 
+            string myInput = Console.ReadLine();
+            int myDecimal = Convert.ToInt32(myInput);
             int myInteger = 0;
             int myBinaryDig = 0;
-            int myBinaryNum = 0; 
-            while (myDeci)
+            string myBinaryNum = " ";
+            string myBinary = " ";
+            while (myDecimal / 2 != 0)
+            {
+                myInteger = myDecimal / 2;
+                myBinaryDig = myDecimal % 2;
+                myBinary = myBinaryDig.ToString();
+                myBinaryNum = myBinaryNum.Insert(0, myBinary);
+                myDecimal = myInteger;
+            }
+            myInteger = myDecimal / 2;
+            myBinaryDig = myDecimal % 2;
+            myBinary = myBinaryDig.ToString();
+           myBinaryNum = myBinaryNum.Insert(0, myBinary);
+            Console.WriteLine("Number in binary is: " + myBinaryNum);
 
-            string myString2 = Console.ReadLine();
-            string newString = myString1.Insert(0, myString2); ///this inserts myString2 at the beginnging on myString1
-            Console.WriteLine(newString);
+            //string myString2 = Console.ReadLine();
+            //string newString = myString1.Insert(0, myString2); ///this inserts myString2 at the beginnging on myString1
+            //Console.WriteLine(newString);
         }
 
         //LET'S USE THE 'STRUCTURED APPROACH' TO PROGRAMMING... A QUICK INTRODUCTION TO SUBROUTINES(functions and procedures)...a.k.a
