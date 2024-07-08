22b5c0306a198c28a07a1c06e08fd2ea3479f7c0
Harlean Kaur <21HKaur@heckgrammar.co.uk>
2024-07-08 09:59:19 +0100

    commit1 9.7.24

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -51,14 +53,45 @@ namespace Y9_DEC_TO_BIN_SKELETON
 
 
             ///SUBROUTINE CONVERSION
-            Console.WriteLine("Please input decimal number from 0 to 255: ");
-            string myInput = Console.ReadLine();
-            string result = convertToBinary(myInput);
-            Console.WriteLine("Number in binary is: " + result);
+            //Console.WriteLine("Please input decimal number from 0 to 255: ");
+            //string myInput = Console.ReadLine();
+            //string result = convertToBinary(myInput);
+            //Console.WriteLine("Number in binary is: " + result);
+             
+            ///Extension
+            Console.WriteLine("Please input a decimal number from 0 to 255 (in digits): ");
+            string userInput = Console.ReadLine();
+            bool my != 0.IsDigit(myInput)
+            int myInput = Convert.ToInt32(userInput);
+            if (
+            {
+
+            }
+            if (-1 < myInput && myInput < 256)
+            {
+                string result = convertToBinary(myInput);
+                Console.WriteLine("Number in binary is: " + result);
+            }
+            else if (myInput != 0)
+            {
+                Console.WriteLine("Your input is not valid");
+                Console.WriteLine("Please re-input a decimal number from 0 to 255 (in digits): ");
+                userInput = Console.ReadLine();
+                myInput = Convert.ToInt32(userInput);
+            }
+            else
+            {
+                Console.WriteLine("Your input is not valid");
+                Console.WriteLine("Please re-input a decimal number from 0 to 255 (in digits): ");
+                userInput = Console.ReadLine();
+                myInput = Convert.ToInt32(userInput);
+            }
+            
         }
 
-        static string convertToBinary(string myInput) 
-         
+        ///Subroutine for Extension
+        static string convertToBinary(int myInput)
+        {
             int myDecimal = Convert.ToInt32(myInput);
             int myInteger = 0;
             int myBinaryDig = 0;
@@ -78,19 +111,43 @@ namespace Y9_DEC_TO_BIN_SKELETON
             myBinaryNum = myBinaryNum.Insert(0, myBinary);
             string finalNum = Convert.ToString(myBinaryNum);
             return finalNum;
-            ///
         }
-   
+        ///
+
+        ///SUBROUTINE FOR CONVERSION
+        //static string convertToBinary(string myInput) 
+        //{ 
+        //    int myDecimal = Convert.ToInt32(myInput);
+        //    int myInteger = 0;
+        //    int myBinaryDig = 0;
+        //    string myBinaryNum = " ";
+        //    string myBinary = " ";
+        //    while (myDecimal / 2 != 0)
+        //    {
+        //        myInteger = myDecimal / 2;
+        //        myBinaryDig = myDecimal % 2;
+        //        myBinary = myBinaryDig.ToString();
+        //        myBinaryNum = myBinaryNum.Insert(0, myBinary);
+        //        myDecimal = myInteger;
+        //    }
+        //    myInteger = myDecimal / 2;
+        //    myBinaryDig = myDecimal % 2;
+        //    myBinary = myBinaryDig.ToString();
+        //    myBinaryNum = myBinaryNum.Insert(0, myBinary);
+        //    string finalNum = Convert.ToString(myBinaryNum);
+        //    return finalNum;
+        //}
+        ///
 
         //LET'S USE THE 'STRUCTURED APPROACH' TO PROGRAMMING... A QUICK INTRODUCTION TO SUBROUTINES(functions and procedures)...a.k.a
         //static void means the function will not return a value so it does not need a data type 
         //...this function DOES return a value so the method must have a data type
-       //static string numberConversion(int number, int numberbase) ///THIS IS A SUBROUTINE 
-       // {
-       //     int num = number + numberbase;
-       //     string result = Convert.ToString(num);
-       //     ///we know the variable result is a string because of static string
-       //     return result; //REMOVE THE RED LINE!
-       // }
+        //static string numberConversion(int number, int numberbase) ///THIS IS A SUBROUTINE 
+        // {
+        //     int num = number + numberbase;
+        //     string result = Convert.ToString(num);
+        //     ///we know the variable result is a string because of static string
+        //     return result; //REMOVE THE RED LINE!
+        // }
     }
 }
